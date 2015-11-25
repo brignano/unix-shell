b5ed0d6a7c763483270c3bf8038272a862a3b81d
CWillbanks <curtiswillbanks@gmail.com>
2015-11-24 23:17:45 -0500

    myshell copy of smsh4.c

---
+++ myshell/myshell.h
@@ -0,0 +1,19 @@
+#define YES 1
+#define No  0
+
+char *next_cmd();
+char **splitline(char *);
+void freelist(char **);
+void *emalloc(size_t);
+void *erealloc(void *, size_t);
+int  execute(char **);
+void fatal(char *, char *, int);
+int  process();
+
+int  VLenviron2table(char **);
+int  VLexport(char *);
+char *VLlookup(char *);
+void VLlist();
+int  VLstore(char *, char *);
+char **VLtable2environ();
+int  VLenviron2table(char **);
